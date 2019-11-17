af67f0b2143e84d06e31a343f13ec974d88d986d
Mehmet Yurtar <mehmetyurtar@gmail.com>
2019-11-17 15:54:46 +0300

    sil style added

--- src/Todo.js
+++ src/Todo.js
@@ -3,8 +3,18 @@ import React from 'react';
 function Todo(props) {
     const {content} = props;
     return (
-        <div>
+        <div style={{
+            margin: "10px"
+        }}>
             {content}
+            <span style={{
+                backgroundColor: "red",
+                fontWeight: "bold",
+                display: "inline-block",
+                marginLeft: "10px",
+                padding: "5px",
+                color: "#fff"
+            }}> Sil</span>
         </div>
     );
 }
